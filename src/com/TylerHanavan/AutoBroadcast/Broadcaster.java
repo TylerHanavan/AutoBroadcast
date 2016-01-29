ade40d975d12cb4cbf4bf23039328cadb241ae4b
Tyler Hanavan <TylerHanavan@gmail.com>
2016-01-29 15:50:16 -0500

    Moved chat color handling to main class
    Now all classes in plugin can easily access chat color interpretation.

--- src/com/TylerHanavan/AutoBroadcast/Broadcaster.java
+++ src/com/TylerHanavan/AutoBroadcast/Broadcaster.java
@@ -1,6 +1,5 @@
 package com.TylerHanavan.AutoBroadcast;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 public class Broadcaster implements Runnable {
@@ -20,7 +19,7 @@ public class Broadcaster implements Runnable {
 
 		for(Player player : this.autoBroadcast.getServer().getOnlinePlayers()){
 			
-			player.sendMessage(this.interpret(this.autoBroadcast.getPrefix() + this.autoBroadcast.getMessages().get(this.count)));
+			player.sendMessage(this.autoBroadcast.interpret(this.autoBroadcast.getPrefix() + this.autoBroadcast.getMessages().get(this.count)));
 			
 		}
 		
@@ -31,18 +30,6 @@ public class Broadcaster implements Runnable {
 			this.count = 0;
 			
 		}
-		
-	}
-	
-	public String interpret(String message){
-		
-		return message.replaceAll("&1", ChatColor.DARK_BLUE + "").replaceAll("&2", ChatColor.DARK_GREEN + "").replaceAll("&3", ChatColor.DARK_AQUA + "")
-				.replaceAll("&4", ChatColor.DARK_RED + "").replaceAll("&5", ChatColor.DARK_PURPLE + "").replaceAll("&6", ChatColor.GOLD + "")
-				.replaceAll("&7", ChatColor.GRAY + "").replaceAll("&8", ChatColor.DARK_GRAY + "").replaceAll("&9", ChatColor.DARK_BLUE + "")
-				.replaceAll("&0", ChatColor.BLACK + "").replaceAll("&a", ChatColor.GREEN + "").replaceAll("&b", ChatColor.AQUA + "")
-				.replaceAll("&c", ChatColor.RED + "").replaceAll("&d", ChatColor.LIGHT_PURPLE + "").replaceAll("&e", ChatColor.YELLOW + "")
-				.replaceAll("&f", ChatColor.WHITE + "").replaceAll("&r", ChatColor.RESET + "").replaceAll("&l", ChatColor.BOLD + "")
-				.replaceAll("&m", ChatColor.STRIKETHROUGH + "").replaceAll("&n", ChatColor.UNDERLINE + "").replaceAll("&k", ChatColor.MAGIC + "");
 		
 	}
 
